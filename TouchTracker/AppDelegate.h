2e549271975989734132494174ad17ebfcdfe277
phoenixbox <shane@quickleft.com>
2013-11-30 20:13:03 -0700

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Shane Rogers on 11/30/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
