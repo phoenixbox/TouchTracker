18750cbb8f65b769284d474151f78d004b53fb7b
phoenixbox <shane@quickleft.com>
2013-11-30 20:50:41 -0700

    draw the lines and implement the clear all method

--- TouchDrawView.m
+++ TouchDrawView.m
@@ -7,7 +7,57 @@
 //
 
 #import "TouchDrawView.h"
+#import "Line.h"
 
 @implementation TouchDrawView
+
+-(id)initWithFrame:(CGRect)r
+{
+    self = [super initWithFrame:r];
+    
+    if (self){
+        linesInProcess = [[NSMutableDictionary alloc]init];
+        
+        completeLines = [[NSMutableArray alloc]init];
+        
+        [self setBackgroundColor:[UIColor whiteColor]];
+        
+        [self setMultipleTouchEnabled:YES];
+    }
+    return self;
+}
+
+-(void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 10.0);
+    CGContextSetLineCap(context,kCGLineCapRound);
+    
+    // Draw complete lines in black
+    [[UIColor blackColor]set];
+    for (Line *line in completeLines){
+        CGContextMoveToPoint(context, [line begin].x,[line begin].y);
+        CGContextAddLineToPoint(context,[line end].x, [line end].y);
+        CGContextStrokePath(context);
+    }
+    
+    // Draw lines in process in red
+    [[UIColor redColor]set];
+    for(NSValue *v in linesInProcess){
+        Line *line = [linesInProcess objectForKey:v];
+        CGContextMoveToPoint(context, [line begin].x, [line begin].y);
+        CGContextAddLineToPoint(context, [line end].x, [line end].y);
+        CGContextStrokePath(context);
+    }
+}
+-(void)clearAll
+{
+    // Clear the collections
+    [linesInProcess removeAllObjects];
+    [completeLines removeAllObjects];
+    
+    // Redraw
+    [self setNeedsDisplay];
+}
 
 @end
