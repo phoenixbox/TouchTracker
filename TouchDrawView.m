e566241fbebffb6d48342cb42e754ff8e519494a
phoenixbox <shane@quickleft.com>
2013-11-30 21:10:47 -0700

    handle the touch events

--- TouchDrawView.m
+++ TouchDrawView.m
@@ -60,4 +60,67 @@
     [self setNeedsDisplay];
 }
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    for(UITouch *t in touches){
+        // Check for double tap - if it is then call clearAll
+        if([t tapCount]>1){
+            [self clearAll];
+            return;
+        }
+        // Use the touch object as the key
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        
+        // Create a line for the value
+        CGPoint loc = [t locationInView:self];
+        Line *newLine = [[Line alloc]init];
+        [newLine setBegin:loc];
+        [newLine setEnd:loc];
+        
+        // Put the pair in the dictionary
+        [linesInProcess setObject:newLine forKey:key];
+    }
+}
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // Update linesInProcess with moved touches
+    for(UITouch *t in touches){
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        
+        // Find the line for this touch
+        Line *line = [linesInProcess objectForKey:key];
+        
+        // Update the line
+        CGPoint loc = [t locationInView:self];
+        [line setEnd:loc];
+    }
+    // Redraw
+    [self setNeedsDisplay];
+}
+-(void)endTouches:(NSSet *)touches
+{
+    // Remove ending touches from dictionary
+    for (UITouch *t in touches){
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        Line *line = [linesInProcess objectForKey:key];
+        
+        // If the interruption is a double tap - line will be nil so add it to the array?
+        if (line){
+            [completeLines addObject:line];
+            [linesInProcess removeObjectForKey:key];
+        }
+    }
+    // Redraw
+    [self setNeedsDisplay];
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self endTouches:touches];
+}
+
+-(void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self endTouches:touches];
+}
 @end
